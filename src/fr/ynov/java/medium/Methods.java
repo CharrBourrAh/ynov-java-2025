eaa30b1a9d49b043697008a78aa4c75d5e87493d
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-04 17:04:57 +0100

    feat(Methods): Methods.java file has been added

---
+++ src/fr/ynov/java/medium/Methods.java
@@ -0,0 +1,10 @@
+package fr.ynov.java.medium;
+
+public class Methods {
+    public static void main(String[] args) {
+
+    }
+    public static int Square(int n) {
+        return n * n;
+    }
+}
