1b1a198a128152508fc799004aea74a0e35d62a6
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-04 16:53:16 +0100

    fix(Scanners): exercise has been fixed

--- src/fr/ynov/java/easy/Scanners.java
+++ src/fr/ynov/java/easy/Scanners.java
@@ -1,17 +1,27 @@
 package fr.ynov.java.easy;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Scanners {
     public static void main(String[] args) {
-        scanMe("Samy", 2);
-        evenOrOdd(4);
-        evenOrOdd(5);
+        scanMe();
+        evenOrOdd();
     }
 
-    public static void scanMe(String name, int age) {
+    public static void scanMe() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name :");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age :");
+        int age = scanner.nextInt();
         System.out.println("your name is : " + name + " and your age is : " + age);
     }
 
-    public static void evenOrOdd(int number) {
+    public static void evenOrOdd() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        number = scanner.nextInt();
         if (number % 2 == 0) {
             System.out.println("even");
         } else {
