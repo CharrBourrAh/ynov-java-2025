16bfbffddb7033f21b8c1ba58c9792c184cf59fc
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-04 16:41:40 +0100

    feat(Scanners): exercise has been completed (both func has been created)

--- src/fr/ynov/java/easy/Scanners.java
+++ src/fr/ynov/java/easy/Scanners.java
@@ -1,4 +1,21 @@
 package fr.ynov.java.easy;
 
 public class Scanners {
+    public static void main(String[] args) {
+        scanMe("Samy", 2);
+        evenOrOdd(4);
+        evenOrOdd(5);
+    }
+
+    public static void scanMe(String name, int age) {
+        System.out.println("your name is : " + name + " and your age is : " + age);
+    }
+
+    public static void evenOrOdd(int number) {
+        if (number % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
 }
