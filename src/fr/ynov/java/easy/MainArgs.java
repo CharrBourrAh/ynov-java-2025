d4777804c7133cb99ebeeaf05f83cce1043a0e57
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-04 17:02:04 +0100

    feat(MainArgs): MainArgs exercise has been completed

--- src/fr/ynov/java/easy/MainArgs.java
+++ src/fr/ynov/java/easy/MainArgs.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class MainArgs {
     public static void main(String[] args) {
-calculator(9,6);
+        calculator(9, 6);
     }
 
     public static void calculator(int a, int b) {
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter an operator sign : ");
-String operator = sc.nextLine();
-switch (operator) {
-    case "+" -> System.out.println(a + b);
-    case "-" -> System.out.println(a - b);
-    case "*" -> System.out.println(a * b);
-    case "/" -> System.out.println(a / b);
-    default -> System.out.println("Invalid operator");
-}
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an operator sign : ");
+        String operator = sc.nextLine();
+        switch (operator) {
+            case "+" -> System.out.println(a + b);
+            case "-" -> System.out.println(a - b);
+            case "*" -> System.out.println(a * b);
+            case "/" -> System.out.println(a / b);
+            default -> System.out.println("Invalid operator");
+        }
     }
 }
