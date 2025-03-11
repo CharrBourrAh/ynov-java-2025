7f70fb9aefc4d2e9892d27c3783443ca932f7281
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-11 16:52:54 +0100

    feat(ReadFile): exercise has been completed

---
+++ src/fr/ynov/java/medium/ReadFile.java
@@ -0,0 +1,61 @@
+package fr.ynov.java.medium;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class ReadFile {
+    public static void readFile(String filePath) {
+        Charset charset = StandardCharsets.US_ASCII;
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(filePath), charset)) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+    }
+
+    public static String[] filesInFolder(String folderPath) {
+        File folder = new File(folderPath);
+        return folder.list();
+    }
+
+    public static void readFilesInFolder(String folderPath) {
+        for (int i = 0; i < filesInFolder(folderPath).length; i++) {
+            readFile(folderPath + "/" + filesInFolder(folderPath)[i]);
+        }
+    }
+
+    public static void createFolderAndFiles(String folderPath) {
+        File folder = new File(folderPath);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        for (int i = 0; i < 5; i++) {
+            String s = "Hello World! " + i;
+            byte[] data = s.getBytes();
+            Path p = Paths.get(folderPath + "/textFile" + i + ".txt");
+
+            try (OutputStream out = new BufferedOutputStream(
+                    Files.newOutputStream(p, CREATE, APPEND))) {
+                out.write(data, 0, data.length);
+            } catch (IOException x) {
+                System.err.println(x);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        readFile("test.txt");
+        createFolderAndFiles("grr paw");
+        readFilesInFolder("grr paw");
+    }
+}
