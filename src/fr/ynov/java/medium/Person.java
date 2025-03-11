60b6bbca98de33d1e69385783da4a3c584d92361
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2025-03-11 15:23:47 +0100

    feat(Person): date of birth have been added

--- src/fr/ynov/java/medium/Person.java
+++ src/fr/ynov/java/medium/Person.java
@@ -1,16 +1,21 @@
 package fr.ynov.java.medium;
 
+import javax.xml.catalog.Catalog;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Person {
     private final String name;
-    private final int age;
+    private final Calendar dateOfBirth;
     private final String gender;
     private final float height;
     private final float weight;
     private final Nationality nationality;
 
-    public Person(String name, int age, String gender, float height, float weight, Nationality nationalityArg) {
+    public Person(String name, Calendar dateOfBirth, String gender, float height, float weight, Nationality nationalityArg) {
         this.name = name;
-        this.age = age;
+        this.dateOfBirth = dateOfBirth;
         this.gender = gender;
         this.height = height;
         this.weight = weight;
@@ -18,17 +23,23 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person = new Person("John", 25, "Male", 1.80f, 80.F, Nationality.France);
+        Calendar cal = Calendar.getInstance();
+        cal.set(1990, Calendar.FEBRUARY, 1);
+        Person person = new Person("John", cal, "Male", 1.80f, 80.F, Nationality.France);
         person.printPerson();
+        System.out.println(person.age());
     }
 
     public void printPerson() {
+
         System.out.println(this.name);
-        System.out.println(this.age);
         System.out.println(this.gender);
         System.out.println(this.height);
         System.out.println(this.weight);
         System.out.println(this.nationality);
     }
 
+    public int age() {
+        return LocalDate.now().getYear() - dateOfBirth.get(Calendar.YEAR);
+    }
 }
